ee5d2c2fd660c0b2655f6a984ee20058aa6cdd30
samuel sutch <sam@sutch.net>
2015-09-11 15:22:09 -0600

    added events to restore controller

--- BreadWallet/BRRestoreViewController.m
+++ BreadWallet/BRRestoreViewController.m
@@ -27,8 +27,10 @@
 #import "BRWalletManager.h"
 #import "BRMnemonic.h"
 #import "NSMutableData+Bitcoin.h"
+#import "BREventManager.h"
 
 #define PHRASE_LENGTH 12
+
 
 @interface BRRestoreViewController ()
 
@@ -37,6 +39,7 @@
 @property (nonatomic, strong) id keyboardObserver, resignActiveObserver;
 
 @end
+
 
 @implementation BRRestoreViewController
 
@@ -95,6 +98,7 @@
 
 - (void)wipeWithPhrase:(NSString *)phrase
 {
+    [BREventManager saveEvent:@"restore:wipe"];
     @autoreleasepool {
         BRWalletManager *m = [BRWalletManager sharedInstance];
         
@@ -102,11 +106,13 @@
         
         if ([[m.sequence masterPublicKeyFromSeed:[m.mnemonic deriveKeyFromPhrase:phrase withPassphrase:nil]]
              isEqual:m.masterPublicKey]) {
+            [BREventManager saveEvent:@"restore:wipe_good_recovery_phrase"];
             [[[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:NSLocalizedString(@"cancel", nil)
               destructiveButtonTitle:NSLocalizedString(@"wipe", nil) otherButtonTitles:nil]
              showInView:[UIApplication sharedApplication].keyWindow];
         }
         else if (phrase) {
+            [BREventManager saveEvent:@"restore:wipe_bad_recovery_phrase"];
             [[[UIAlertView alloc] initWithTitle:@"" message:NSLocalizedString(@"recovery phrase doesn't match", nil)
               delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", nil) otherButtonTitles:nil] show];
         }
@@ -118,6 +124,7 @@
 
 - (IBAction)cancel:(id)sender
 {
+    [BREventManager saveEvent:@"restore:cancel"];
     [self.navigationController.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -135,7 +142,7 @@
         invalid = set.invertedSet;
     });
 
-    @autoreleasepool {  // @autoreleasepool ensures sensitive data will be dealocated immediately
+    @autoreleasepool {  // @autoreleasepool ensures sensitive data will be deallocated immediately
         if ([textView.text rangeOfString:@"\n"].location == NSNotFound) return; // not done entering phrase
     
         BRWalletManager *m = [BRWalletManager sharedInstance];
@@ -160,6 +167,7 @@
             [self performSelector:@selector(wipeWithPhrase:) withObject:phrase afterDelay:0.0];
         }
         else if (incorrect) {
+            [BREventManager saveEvent:@"restore:invalid_word"];
             textView.selectedRange = [textView.text.lowercaseString rangeOfString:incorrect];
         
             [[[UIAlertView alloc] initWithTitle:@""
@@ -168,12 +176,14 @@
              show];
         }
         else if (a.count != PHRASE_LENGTH) {
+            [BREventManager saveEvent:@"restore:invalid_num_words"];
             [[[UIAlertView alloc] initWithTitle:@""
               message:[NSString stringWithFormat:NSLocalizedString(@"recovery phrase must have %d words", nil),
                        PHRASE_LENGTH] delegate:nil cancelButtonTitle:NSLocalizedString(@"ok", nil)
               otherButtonTitles:nil] show];
         }
         else if (isLocal && ! [m.mnemonic phraseIsValid:phrase]) {
+            [BREventManager saveEvent:@"restore:bad_phrase"];
             [[[UIAlertView alloc] initWithTitle:@"" message:NSLocalizedString(@"bad recovery phrase", nil) delegate:nil
               cancelButtonTitle:NSLocalizedString(@"ok", nil) otherButtonTitles:nil] show];
         }
@@ -185,6 +195,7 @@
             //TODO: offer the user an option to move funds to a new seed if their wallet device was lost or stolen
             m.seedPhrase = phrase;
             textView.text = nil;
+            [BREventManager saveEvent:@"restore:set_pin"];
             
             [self.navigationController.presentingViewController dismissViewControllerAnimated:YES completion:^{
                 [m performSelector:@selector(setPin) withObject:nil afterDelay:0.3];
