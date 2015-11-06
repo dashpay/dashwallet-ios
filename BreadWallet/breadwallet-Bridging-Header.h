8d0868ed9c37ad41758cecd1f39be136632820c4
Samuel Sutch <sam@sutch.net>
2015-11-06 14:11:44 -0700

    add bridging header back

---
+++ BreadWallet/breadwallet-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "BRAppDelegate.h"
+#import "BRWalletManager.h"
+#import "BRKey.h"
+#import "NSData+Bitcoin.h"
+#import "NSString+Bitcoin.h"
+#import "NSMutableData+Bitcoin.h"
