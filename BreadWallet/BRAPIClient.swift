d9610eb6a54d5dae8a56b1efc1917a8eb8542fd4
Samuel Sutch <sam@sutch.net>
2015-11-05 21:12:16 -0700

    dataTask wrapper, fetch token

--- BreadWallet/BRAPIClient.swift
+++ BreadWallet/BRAPIClient.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+let BRAPIClientErrorDomain = "BRApiClientErrorDomain"
+
 extension String {
     static var urlQuoteCharacterSet: NSCharacterSet {
         let cset = NSMutableCharacterSet.URLQueryAllowedCharacterSet().mutableCopy() as! NSMutableCharacterSet
@@ -21,11 +23,44 @@ extension String {
     }
 }
 
+func getHeaderValue(k: String, d: Dictionary<NSObject, AnyObject>) -> String? {
+    if let v = d[k] as? String { // short path: attempt to get the header directly
+        return v
+    }
+    let lkKey = k.lowercaseString // long path: compare lowercase keys
+    for (lk, lv) in d {
+        if lk is String {
+            let lks = lk as! String
+            if lks == lkKey {
+                if let lvs = lv as? String {
+                    return lvs
+                }
+            }
+        }
+    }
+    return nil
+}
+
+func getPublicKey() -> NSData? {
+    let man = BRWalletManager.sharedInstance()
+    return nil
+}
+
+func isBreadChallenge(r: NSHTTPURLResponse) -> Bool {
+    if let challenge = getHeaderValue("www-authenticate", d: r.allHeaderFields) {
+        if challenge.lowercaseString.hasPrefix("bread") {
+            return true
+        }
+    }
+    return false
+}
+
 @objc class BRAPIClient: NSObject, NSURLSessionDelegate, NSURLSessionTaskDelegate {
     var session: NSURLSession!
     var queue: NSOperationQueue!
     var logEnabled = true
-    var host = "api.breadwallet.com"
+    var proto = "http"
+    var host = "localhost:8009"
     var baseUrl: String!
     
     // the singleton
@@ -36,7 +71,7 @@ extension String {
         queue = NSOperationQueue()
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
         session = NSURLSession(configuration: config, delegate: self, delegateQueue: queue)
-        baseUrl = "https://\(host)"
+        baseUrl = "\(proto)://\(host)"
     }
     
     func log(format: String, args: CVarArgType...) {
@@ -46,6 +81,8 @@ extension String {
         let s = String(format: format, arguments: args)
         print("[BRAPIClient] \(s)")
     }
+    
+    // MARK: Networking functions
     
     // Constructs a full NSURL for a given path and url parameters
     func url(path: String, args: Dictionary<String, String>? =  nil) -> NSURL {
@@ -62,14 +99,86 @@ extension String {
         }
     }
     
+    func dataTaskWithRequest(
+        request: NSURLRequest,
+        handler: (NSData?, NSHTTPURLResponse?, NSError?) -> Void) -> NSURLSessionDataTask {
+            return session.dataTaskWithRequest(request) { (data, resp, err) -> Void in
+                if let httpResp = resp as? NSHTTPURLResponse {
+                    if isBreadChallenge(httpResp) {
+                        self.getToken({ (err) -> Void in
+                            handler(data, httpResp, err)
+                        })
+                    } else {
+                        handler(data, httpResp, err)
+                    }
+                }
+            }
+    }
+    
+    func getToken(handler: (NSError?) -> Void) -> Void {
+        let req = NSMutableURLRequest(URL: url("/token"))
+        req.HTTPMethod = "POST"
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        req.setValue("application/json", forHTTPHeaderField: "Accept")
+        let reqJson = [
+            "pubKey": "",
+            "deviceID": ""
+        ]
+        do {
+            let dat = try NSJSONSerialization.dataWithJSONObject(reqJson, options: .PrettyPrinted)
+            req.HTTPBody = dat
+        } catch (let e) {
+            log("JSON Serialization error \(e)")
+        }
+        let task = session.dataTaskWithRequest(req) { (data, resp, err) -> Void in
+            if let httpResp = resp as? NSHTTPURLResponse {
+                if httpResp.statusCode != 200 {
+                    if let data = data {
+                        if let s = String(data: data, encoding: NSUTF8StringEncoding) {
+                            self.log("Token error: \(s)")
+                        }
+                    }
+                    return handler(NSError(
+                        domain: BRAPIClientErrorDomain,
+                        code: httpResp.statusCode,
+                        userInfo: [
+                            NSLocalizedDescriptionKey:
+                            NSLocalizedString("Unable to retrieve API token", comment: "")]))
+                }
+            }
+            if let data = data {
+                do {
+                    let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+                    self.log("POST /token: \(json)")
+                } catch (let e) {
+                    self.log("JSON Deserialization error \(e)")
+                }
+            }
+            handler(err)
+        }
+        task.resume()
+    }
+    
+    // MARK: URLSession Delegate
+    
     func URLSession(session: NSURLSession, didBecomeInvalidWithError error: NSError?) {
         log("URLSession didBecomeInvalidWithError: \(error)")
     }
     
     func URLSession(
         session: NSURLSession,
+        task: NSURLSessionTask,
         didReceiveChallenge challenge: NSURLAuthenticationChallenge,
         completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+            log("URLSession task \(task) didReceivechallenge \(challenge.protectionSpace)")
+            
+    }
+    
+    func URLSession(
+        session: NSURLSession,
+        didReceiveChallenge challenge: NSURLAuthenticationChallenge,
+        completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+            log("URLSession didReceiveChallenge \(challenge)")
             // handle HTTPS authentication
             if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
                 if challenge.protectionSpace.host == host && challenge.protectionSpace.serverTrust != nil {
@@ -80,6 +189,8 @@ extension String {
                 }
             }
     }
+    
+    // MARK: API Functions
     
     // Fetches the /v1/fee-per-kb endpoint
     func feePerKb(handler: (feePerKb: uint_fast64_t, error: String?) -> Void) {
@@ -113,10 +224,13 @@ extension String {
     
     func me() {
         let req = NSURLRequest(URL: url("/me"))
-        let task = session.dataTaskWithRequest(req, completionHandler: { (data, resp, err) -> Void in
-            let ds = String(data: data!, encoding: NSUTF8StringEncoding)
-            self.log("GET /me: \(ds)")
-        })
+        let task = dataTaskWithRequest(req) { (data, resp, err) -> Void in
+            if let data = data {
+                if let s = String(data: data, encoding: NSUTF8StringEncoding) {
+                    self.log("GET /me: \(s)")
+                }
+            }
+        }
         task.resume()
     }
 }
--- BreadWallet/swift-bridge-header.h
+++ BreadWallet/swift-bridge-header.h
@@ -1,12 +1,17 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  swift-bridge-header.h
+//  BreadWallet
 //
-
+//  Created by Samuel Sutch on 11/5/15.
+//  Copyright © 2015 Aaron Voisine. All rights reserved.
+//
 
 #import "BRKey.h"
 #import "BRWallet.h"
+#import "BRWalletManager.h"
 #import "BRPeer.h"
 #import "BRPeerManager.h"
 #import "NSString+Bitcoin.h"
 #import "NSData+Bitcoin.h"
 #import "NSMutableData+Bitcoin.h"
+
