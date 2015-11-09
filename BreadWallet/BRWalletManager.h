98db6600fa00432480e9d93def8e3218d61a2476
Samuel Sutch <sam@sutch.net>
2015-11-09 16:00:00 -0700

    add nullability annotations to BRWalletManager
    improves interoperability with swift

--- BreadWallet/BRWalletManager.h
+++ BreadWallet/BRWalletManager.h
@@ -44,47 +44,47 @@
 
 @interface BRWalletManager : NSObject<UIAlertViewDelegate, UITextFieldDelegate, UITextViewDelegate>
 
-@property (nonatomic, readonly) BRWallet *wallet;
+@property (nonatomic, readonly) BRWallet * _Nullable wallet;
 @property (nonatomic, readonly) BOOL noWallet; // true if keychain is available and we know that no wallet exists on it
 @property (nonatomic, readonly) BOOL watchOnly; // true if this is a "watch only" wallet with no signing ability
-@property (nonatomic, strong) id<BRKeySequence> sequence;
-@property (nonatomic, strong) id<BRMnemonic> mnemonic;
-@property (nonatomic, readonly) NSData *masterPublicKey; // master public key used to generate wallet addresses
-@property (nonatomic, copy) NSString *seedPhrase; // requesting seedPhrase will trigger authentication
+@property (nonatomic, strong) id<BRKeySequence> _Nullable sequence;
+@property (nonatomic, strong) id<BRMnemonic> _Nullable mnemonic;
+@property (nonatomic, readonly) NSData * _Nullable masterPublicKey; // master public key used to generate wallet addresses
+@property (nonatomic, copy) NSString * _Nullable seedPhrase; // requesting seedPhrase will trigger authentication
 @property (nonatomic, readonly) NSTimeInterval seedCreationTime; // interval since refrence date, 00:00:00 01/01/01 GMT
 @property (nonatomic, readonly) NSTimeInterval secureTime; // last known time from an ssl server connection
 @property (nonatomic, assign) uint64_t spendingLimit; // amount that can be spent using touch id without pin entry
-@property (nonatomic, readonly) NSString *authPrivateKey; // private key for signing authenticated api calls
+@property (nonatomic, readonly) NSString * _Nullable authPrivateKey; // private key for signing authenticated api calls
 @property (nonatomic, readonly, getter=isTouchIdEnabled) BOOL touchIdEnabled; // true if touch id is enabled
 @property (nonatomic, readonly, getter=isPasscodeEnabled) BOOL passcodeEnabled; // true if device passcode is enabled
 @property (nonatomic, assign) BOOL didAuthenticate; // true if the user authenticated after this was last set to false
-@property (nonatomic, readonly) NSNumberFormatter *format; // bitcoin currency formatter
-@property (nonatomic, readonly) NSNumberFormatter *localFormat; // local currency formatter
-@property (nonatomic, copy) NSString *localCurrencyCode; // local currency ISO code
+@property (nonatomic, readonly) NSNumberFormatter * _Nullable format; // bitcoin currency formatter
+@property (nonatomic, readonly) NSNumberFormatter * _Nullable localFormat; // local currency formatter
+@property (nonatomic, copy) NSString * _Nullable localCurrencyCode; // local currency ISO code
 @property (nonatomic, readonly) double localCurrencyPrice; // exchange rate in local currency units per bitcoin
-@property (nonatomic, readonly) NSArray *currencyCodes; // list of supported local currency codes
-@property (nonatomic, readonly) NSArray *currencyNames; // names for local currency codes
+@property (nonatomic, readonly) NSArray * _Nullable currencyCodes; // list of supported local currency codes
+@property (nonatomic, readonly) NSArray * _Nullable currencyNames; // names for local currency codes
 
-+ (instancetype)sharedInstance;
++ (instancetype _Nullable)sharedInstance;
 
-- (NSString *)generateRandomSeed; // generates a random seed, saves to keychain and returns the associated seedPhrase
-- (NSData *)seedWithPrompt:(NSString *)authprompt forAmount:(uint64_t)amount; // authenticates user and returns seed
-- (NSString *)seedPhraseWithPrompt:(NSString *)authprompt; // authenticates user and returns seedPhrase
-- (BOOL)authenticateWithPrompt:(NSString *)authprompt andTouchId:(BOOL)touchId; // prompts user to authenticate
+- (NSString * _Nullable)generateRandomSeed; // generates a random seed, saves to keychain and returns the associated seedPhrase
+- (NSData * _Nullable)seedWithPrompt:(NSString * _Nullable)authprompt forAmount:(uint64_t)amount; // authenticates user and returns seed
+- (NSString * _Nullable)seedPhraseWithPrompt:(NSString * _Nullable)authprompt; // authenticates user and returns seedPhrase
+- (BOOL)authenticateWithPrompt:(NSString * _Nullable)authprompt andTouchId:(BOOL)touchId; // prompts user to authenticate
 - (BOOL)setPin; // prompts the user to set or change wallet pin and returns true if the pin was successfully set
 
 // queries chain.com and calls the completion block with unspent outputs for the given address
-- (void)utxosForAddress:(NSString *)address
-completion:(void (^)(NSArray *utxos, NSArray *amounts, NSArray *scripts, NSError *error))completion;
+- (void)utxosForAddress:(NSString * _Nonnull)address
+completion:(void (^ _Nonnull)(NSArray * _Nonnull utxos, NSArray * _Nonnull amounts, NSArray * _Nonnull scripts, NSError * _Null_unspecified error))completion;
 
 // given a private key, queries chain.com for unspent outputs and calls the completion block with a signed transaction
 // that will sweep the balance into wallet (doesn't publish the tx)
-- (void)sweepPrivateKey:(NSString *)privKey withFee:(BOOL)fee
-completion:(void (^)(BRTransaction *tx, uint64_t fee, NSError *error))completion;
+- (void)sweepPrivateKey:(NSString * _Nonnull)privKey withFee:(BOOL)fee
+completion:(void (^ _Nonnull)(BRTransaction * _Nonnull tx, uint64_t fee, NSError * _Null_unspecified error))completion;
 
-- (int64_t)amountForString:(NSString *)string;
-- (NSString *)stringForAmount:(int64_t)amount;
-- (int64_t)amountForLocalCurrencyString:(NSString *)string;
-- (NSString *)localCurrencyStringForAmount:(int64_t)amount;
+- (int64_t)amountForString:(NSString * _Nonnull)string;
+- (NSString * _Nonnull)stringForAmount:(int64_t)amount;
+- (int64_t)amountForLocalCurrencyString:(NSString * _Nonnull)string;
+- (NSString * _Nonnull)localCurrencyStringForAmount:(int64_t)amount;
 
 @end
