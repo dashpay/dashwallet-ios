7a54a53e53a907c2c0e88dd95a9bf45196a0d225
Samuel Sutch <sam@sutch.net>
2015-12-27 18:19:29 -0700

    update watch immediately - dont wait until poll

--- WatchApp Extension/ExtensionDelegate.swift
+++ WatchApp Extension/ExtensionDelegate.swift
@@ -26,17 +26,16 @@
 import WatchKit
 
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
-
     func applicationDidFinishLaunching() {
         // Perform any final initialization of your application.
     }
 
     func applicationDidBecomeActive() {
         BRAWWatchDataManager.sharedInstance.setupTimer()
+        BRAWWatchDataManager.sharedInstance.requestAllData()
     }
 
     func applicationWillResignActive() {
         BRAWWatchDataManager.sharedInstance.destoryTimer()
     }
-
 }
