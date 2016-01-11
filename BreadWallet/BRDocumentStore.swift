7543dfd2a88a7f9c43cc6f810644e6b47bc900a5
Samuel Sutch <sam@sutch.net>
2016-01-10 21:24:06 -0700

    document store with get and set

--- BreadWallet/BRDocumentStore.swift
+++ BreadWallet/BRDocumentStore.swift
@@ -65,11 +65,17 @@ public class AsyncResult<T> {
     }
 }
 
-public protocol Document {
+public protocol DocumentLoading {
     init(json: AnyObject?) throws
+    func dump() throws -> NSData
 }
 
-public struct DatabaseInfo: Document {
+public protocol Document: DocumentLoading {
+    var _id: String { get set }
+    var _rev: String { get set }
+}
+
+public struct DatabaseInfo: DocumentLoading {
     let dbName: String
     let docCount: Int
     let diskSize: Int
@@ -91,6 +97,20 @@ public struct DatabaseInfo: Document {
         updateSeq = (doc["update_seq"] as! NSNumber).integerValue
         compactRunning = (doc["update_seq"] as! NSNumber).boolValue
         committedUpdateSeq = (doc["committed_update_seq"] as! NSNumber).integerValue
+    }
+    
+    public func dump() throws -> NSData {
+        var d = [String: AnyObject]()
+        d["db_name"] = dbName
+        d["doc_count"] = NSNumber(integer: docCount)
+        d["disk_size"] = NSNumber(integer: diskSize)
+        d["data_size"] = NSNumber(integer: dataSize)
+        d["doc_del_count"] = NSNumber(integer: docDelCount)
+        d["purge_seq"] = NSNumber(integer: purgeSeq)
+        d["update_seq"] = NSNumber(integer: updateSeq)
+        d["compact_running"] = NSNumber(bool: compactRunning)
+        d["committed_update_seq"] = NSNumber(integer: committedUpdateSeq)
+        return try NSJSONSerialization.dataWithJSONObject(d as NSDictionary, options: [])
     }
 }
 
@@ -117,10 +137,10 @@ public protocol ReplicationClient {
     func ensureFullCommit() -> AsyncResult<Bool>
 
     // retrieve a document from the database
-    func get<T>(id: String, options: [String: [String]]?) -> AsyncResult<T?>
+    func get<T: Document>(id: String, options: [String: [String]]?, returning: T.Type) -> AsyncResult<T?>
     
     // put a document to the database
-    func put<T: Document>(doc: T, options: [String: [String]]?) -> AsyncResult<Bool>
+    func put<T: Document>(doc: T, options: [String: [String]]?, returning: T.Type) -> AsyncResult<T>
     
     // fetch all documents
     func allDocs<T: Document>(options: [String: [String]]?) -> AsyncResult<[T]>
@@ -223,13 +243,76 @@ public class RemoteCouchDB: ReplicationClient {
         return result
     }
     
-    public func get<T>(id: String, options: [String : [String]]?) -> AsyncResult<T?> {
+    public func get<T: Document>(id: String, options: [String : [String]]?, returning: T.Type) -> AsyncResult<T?> {
         let result = AsyncResult<T?>()
+        
+        let req = NSMutableURLRequest(URL: NSURL(string: url + "/" + id + buildQueryString(options))!)
+        req.setValue("application/json", forHTTPHeaderField: "Accept")
+        NSURLSession.sharedSession().dataTaskWithRequest(req) { (data, resp, err) -> Void in
+            if let resp = resp as? NSHTTPURLResponse {
+                if let data = data where resp.statusCode == 200 {
+                    do {
+                        let j = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+                        let doc = try T(json: j)
+                        result.succeed(doc)
+                    } catch let e {
+                        print("[RemoteCouchDB] error loading get json \(e)")
+                        result.error(-1001, message: "Error loading document json: \(e)")
+                    }
+                } else if resp.statusCode == 404 {
+                    result.succeed(nil)
+                } else {
+                    result.error(
+                        resp.statusCode, message: NSHTTPURLResponse.localizedStringForStatusCode(resp.statusCode))
+                }
+            } else {
+                result.error(-1001, message: "Error running get request \(err?.debugDescription)")
+            }
+        }.resume()
+        
         return result
     }
     
-    public func put<T : Document>(doc: T, options: [String : [String]]?) -> AsyncResult<Bool> {
-        let result = AsyncResult<Bool>()
+    public func put<T : Document>(doc: T, options: [String : [String]]?, returning: T.Type) -> AsyncResult<T> {
+        let result = AsyncResult<T>()
+        var docJson: NSData? = nil
+        do {
+            docJson = try doc.dump()
+        } catch let e {
+            print("[RemoteCouchDB] error dumping json")
+            result.error(-1001, message: "JSON Dumping Error: \(e)")
+            return result
+        }
+        
+        let req = NSMutableURLRequest(URL: NSURL(string: url + "/" + doc._id + buildQueryString(options))!)
+        req.HTTPMethod = "PUT"
+        req.HTTPBody = docJson
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        req.setValue("application/json", forHTTPHeaderField: "Accept")
+        NSURLSession.sharedSession().dataTaskWithRequest(req) { (data, resp, err) -> Void in
+            if let resp = resp as? NSHTTPURLResponse {
+                if let data = data where resp.statusCode == 201 {
+                    do {
+                        let j = try NSJSONSerialization.JSONObjectWithData(data, options: [])
+                        let rev = j["rev"] as! NSString
+                        var d = doc
+                        d._rev = rev as String
+                        result.succeed(d)
+                    } catch let e {
+                        print("[RemoteCouchDB] error loading put response \(e)")
+                        result.error(-1001, message: "Error loading put response \(e)")
+                    }
+                } else {
+                    let s = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                    print("[RemoteCouchDB] put error resp: \(resp) returned: \(s)")
+                    result.error(
+                        resp.statusCode, message: NSHTTPURLResponse.localizedStringForStatusCode(resp.statusCode))
+                }
+            } else {
+                result.error(-1001, message: "Error sending put \(err?.debugDescription)")
+            }
+        }.resume()
+        
         return result
     }
     
@@ -246,6 +329,24 @@ public class RemoteCouchDB: ReplicationClient {
     public func revsDiff<T : Document>(revs: [RevisionInfo<T>], options: [String : [String]]?) -> AsyncResult<[RevisionInfo<T>]> {
         let result = AsyncResult<[RevisionInfo<T>]>()
         return result
+    }
+    
+    private func buildQueryString(options: [String: [String]]?) -> String {
+        var s = ""
+        if let options = options where options.count > 0 {
+            s = "?"
+            var i = 0
+            for (k, vals) in options {
+                for v in vals {
+                    if i != 0 {
+                        s += "&"
+                    }
+                    i++
+                    s += "\(k.urlEscapedString)=\(v.urlEscapedString)"
+                }
+            }
+        }
+        return s
     }
 }
 
--- BreadWalletTests/BRDocumentStoreTests.swift
+++ BreadWalletTests/BRDocumentStoreTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import breadwallet
 
-class BRDocumentStoreTests: XCTestCase {
+class BRDocumentStoreStartTests: XCTestCase {
     var cli: ReplicationClient!
     var dbName: String!
     
@@ -79,6 +79,97 @@ class BRDocumentStoreTests: XCTestCase {
                 return dbInfo
             })
             return didSucceed
+        })
+        waitForExpectationsWithTimeout(5, handler: nil)
+    }
+}
+
+class TestDocument: Document {
+    var _id: String
+    var _rev: String
+    var aString: String
+    
+    required init(json: AnyObject?) throws {
+        let d = json as! NSDictionary
+        _id = d["_id"] as! String
+        _rev = d["_rev"] as! String
+        aString = d["a_string"] as! String
+    }
+    
+    init(name: String, value: String) {
+        _id = name
+        aString = value
+        _rev = ""
+    }
+    
+    func dump() throws -> NSData {
+        var d = [String: AnyObject]()
+        d["_id"] = _id
+        if _rev != "" {
+            d["_rev"] = _rev
+        }
+        d["a_string"] = aString
+        return try NSJSONSerialization.dataWithJSONObject(d as NSDictionary, options: [])
+    }
+}
+
+class BRDocumentStoreTests: XCTestCase {
+    var cli: ReplicationClient!
+    var dbName: String!
+    
+    override func setUp() {
+        dbName = "yyz" + NSUUID().UUIDString.lowercaseString
+        cli = RemoteCouchDB(url: "http://localhost:5984/" + dbName)
+        let exp = expectationWithDescription("db setup")
+        cli.create().success(AsyncCallback<Bool> { didSucceed in
+            XCTAssert(didSucceed)
+            exp.fulfill()
+            return didSucceed
+        })
+        waitForExpectationsWithTimeout(5, handler: nil)
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        cli = nil
+        dbName = nil
+        super.tearDown()
+    }
+    
+    func testGet404() {
+        let exp = expectationWithDescription("get 404")
+        cli.get("hello404", options: nil, returning: TestDocument.self).success(AsyncCallback<TestDocument?> { d in
+            XCTAssert(d == nil)
+            exp.fulfill()
+            return d
+        })
+        waitForExpectationsWithTimeout(5, handler: nil)
+    }
+    
+    func testPutSuccess() {
+        let exp = expectationWithDescription("put success")
+        let doc = TestDocument(name: "helloPut", value: "a value")
+        cli.put(doc, options: nil, returning: TestDocument.self).success(AsyncCallback<TestDocument> { d in
+            XCTAssert(d._rev != "")
+            exp.fulfill()
+            return d
+        })
+        waitForExpectationsWithTimeout(5, handler: nil)
+    }
+    
+    func testPutThenGet() {
+        let exp = expectationWithDescription("put then get")
+        let doc = TestDocument(name: "helloPut", value: "a value")
+        cli.put(doc, options: nil, returning: TestDocument.self).success(AsyncCallback<TestDocument> { d in
+            XCTAssert(d._rev != "")
+            let rev = d._rev
+            self.cli.get("helloPut", options: nil, returning: TestDocument.self).success(AsyncCallback<TestDocument?> { d in
+                XCTAssert(d!._rev == rev)
+                XCTAssert(d!.aString == "a value")
+                exp.fulfill()
+                return d
+            })
+            return d
         })
         waitForExpectationsWithTimeout(5, handler: nil)
     }
