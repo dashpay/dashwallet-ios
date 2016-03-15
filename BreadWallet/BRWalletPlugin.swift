f9d477c6bb45f9e8bc00facb2eb1f826ebddb592
Samuel Sutch <sam@sutch.net>
2016-03-15 15:03:12 -0600

    add currency formatting API

--- BreadWallet/BRWalletPlugin.swift
+++ BreadWallet/BRWalletPlugin.swift
@@ -19,6 +19,25 @@ import Foundation
         router.get("/_wallet/info") { (request, match) -> BRHTTPResponse in
             return try BRHTTPResponse(request: request, code: 200, json: self.walletInfo())
         }
+        
+        router.get("/_wallet/format") { (request, match) -> BRHTTPResponse in
+            if let amounts = request.query["amount"] where amounts.count > 0 {
+                let amount = amounts[0]
+                var intAmount: Int64 = 0
+                if amount.containsString(".") { // assume full bitcoins
+                    if let x = Float(amount) {
+                        intAmount = Int64(x * 100000000.0)
+                    }
+                } else {
+                    if let x = Int64(amount) {
+                        intAmount = x
+                    }
+                }
+                return try BRHTTPResponse(request: request, code: 200, json: self.currencyFormat(intAmount))
+            } else {
+                return BRHTTPResponse(request: request, code: 400)
+            }
+        }
     }
     
     // MARK: - basic wallet functions
@@ -28,6 +47,13 @@ import Foundation
         d["no_wallet"] = manager.noWallet
         d["watch_only"] = manager.watchOnly
         d["receive_address"] = manager.wallet?.receiveAddress
+        return d
+    }
+    
+    func currencyFormat(amount: Int64) -> [String: AnyObject] {
+        var d = [String: AnyObject]()
+        d["local_currency_amount"] = manager.localCurrencyStringForAmount(Int64(amount))
+        d["currency_amount"] = manager.stringForAmount(amount)
         return d
     }
     
