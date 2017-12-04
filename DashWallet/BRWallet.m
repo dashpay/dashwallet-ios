2e7498755b23f484d094da9c63e4b4ca2735a6ff
Quantum Explorer <quantumexplorer@dashpay.io>
2017-12-04 17:16:54 +0700

    better fee calculation

--- DashWallet/BRWallet.m
+++ DashWallet/BRWallet.m
@@ -1023,10 +1023,14 @@ static NSUInteger txAddressIndex(BRTransaction *tx, NSArray *chain) {
     if (isInstant) {
         return TX_FEE_PER_INPUT*inputCount;
     } else {
-        uint64_t standardFee = ((size + 999)/1000)*TX_FEE_PER_KB, // standard fee based on tx size rounded up to nearest kb
-        fee = (((size*self.feePerKb/1000) + 99)/100)*100; // fee using feePerKb, rounded up to nearest 100 satoshi
-        
+        uint64_t standardFee = ((size + 999)/1000)*TX_FEE_PER_KB; // standard fee based on tx size rounded up to nearest kb
+#if (!!FEE_PER_KB_URL)
+        uint64_t fee = (((size*self.feePerKb/1000) + 99)/100)*100; // fee using feePerKb, rounded up to nearest 100 satoshi
         return (fee > standardFee) ? fee : standardFee;
+#else
+        return standardFee;
+#endif
+        
     }
 }
 
