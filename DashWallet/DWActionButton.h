61868d711faeca1b574da8dab5efca20a94da365
Quantum Explorer <quantum@dash.org>
2018-08-10 03:10:19 +0700

    Action buttons highlighted state

---
+++ DashWallet/DWActionButton.h
@@ -0,0 +1,13 @@
+//
+//  DWActionButton.h
+//  dashwallet
+//
+//  Created by Sam Westrich on 8/10/18.
+//  Copyright © 2018 Aaron Voisine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DWActionButton : UIButton
+
+@end
