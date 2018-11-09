e7607dba74e5e459fdc0eca9f7e2aeaf52ac60b1
Andrew Podkovyrin <podkovyrin@gmail.com>
2018-11-10 01:12:59 +0300

    Migration manager

---
+++ DashWallet/DataMigration/DWDataMigrationManager.h
@@ -0,0 +1,21 @@
+//
+//  DWDataMigrationManager.h
+//  dashwallet
+//
+//  Created by Andrew Podkovyrin on 08/11/2018.
+//  Copyright © 2018 Dash Core. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DWDataMigrationManager : NSObject
+
++ (instancetype)sharedInstance;
+
+- (void)migrate:(void (^)(BOOL completed))completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
