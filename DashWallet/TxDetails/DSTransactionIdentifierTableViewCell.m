e8349b08cc5808ca292eb3219c6a6210126a3411
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-03-05 17:48:24 +0300

    Display extended info for special txs

---
+++ DashWallet/TxDetails/DSTransactionIdentifierTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DSTransactionIdentifierTableViewCell.m
+//  DashSync_Example
+//
+//  Created by Sam Westrich on 7/22/18.
+//  Copyright © 2018 Dash Core Group. All rights reserved.
+//
+
+#import "DSTransactionIdentifierTableViewCell.h"
+
+@implementation DSTransactionIdentifierTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ DashWallet/TxDetails/DSTransactionStatusTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  DSTransactionStatusTableViewCell.h
+//  DashSync_Example
+//
+//  Created by Sam Westrich on 7/22/18.
+//  Copyright © 2018 Dash Core Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BRCopyLabel.h"
+
+@interface DSTransactionStatusTableViewCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet BRCopyLabel *statusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *moreInfoLabel;
+@property (strong, nonatomic) IBOutlet BRCopyLabel *titleLabel;
+
+@end
