6fed7e2648d25ffccaef918a545662ed27113907
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-07-17 10:07:12 +0300

    Support drag-n-drop in verification

--- DashWallet/Sources/UI/Backup/Seed/Views/SeedPhrase/DWSeedPhraseView.m
+++ DashWallet/Sources/UI/Backup/Seed/Views/SeedPhrase/DWSeedPhraseView.m
@@ -55,12 +55,41 @@ static BOOL MasksToBounds(DWSeedPhraseType type) {
     }
 }
 
-@interface DWSeedPhraseView () <DWSeedPhraseViewLayoutDataSource>
+#pragma mark - Drag & Drop Helper
+
+@interface DWDragItemLocalObject : NSObject
+
+@property (readonly, nonatomic, strong) DWSeedWordModel *wordModel;
+@property (readonly, nullable, nonatomic, weak) id<DWSeedPhraseViewDelegate> externalDelegate;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+@implementation DWDragItemLocalObject
+
+- (instancetype)initWithWord:(DWSeedWordModel *)wordModel delegate:(id<DWSeedPhraseViewDelegate>)delegate {
+    self = [super init];
+    if (self) {
+        _wordModel = wordModel;
+        _externalDelegate = delegate;
+    }
+    return self;
+}
+
+@end
+
+#pragma mark - View
+
+@interface DWSeedPhraseView () <DWSeedPhraseViewLayoutDataSource,
+                                UIDragInteractionDelegate,
+                                UIDropInteractionDelegate>
 
 @property (readonly, nonatomic, assign) DWSeedPhraseType type;
 
 @property (nullable, nonatomic, copy) NSArray<DWSeedWordView *> *wordViews;
 @property (nullable, nonatomic, strong) DWSeedPhraseViewLayout *layout;
+@property (nonatomic, assign) BOOL hasActiveDragInteractions;
 
 @end
 
@@ -75,6 +104,11 @@ static BOOL MasksToBounds(DWSeedPhraseType type) {
 
         self.layer.cornerRadius = CornerRadius(type);
         self.layer.masksToBounds = MasksToBounds(type);
+
+        if (type == DWSeedPhraseType_Verify) {
+            UIDropInteraction *dropInteraction = [[UIDropInteraction alloc] initWithDelegate:self];
+            [self addInteraction:dropInteraction];
+        }
 
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(contentSizeCategoryDidChangeNotification:)
@@ -121,16 +155,25 @@ static BOOL MasksToBounds(DWSeedPhraseType type) {
     [self.wordViews makeObjectsPerformSelector:@selector(removeFromSuperview)];
     self.wordViews = nil;
 
+    DWSeedPhraseType type = self.type;
+
     NSMutableArray<DWSeedWordView *> *wordViews = [NSMutableArray array];
     for (DWSeedWordModel *wordModel in self.model.words) {
-        DWSeedWordView *wordView = [[DWSeedWordView alloc] initWithType:self.type];
+        DWSeedWordView *wordView = [[DWSeedWordView alloc] initWithType:type];
         wordView.model = wordModel;
-        if (self.type == DWSeedPhraseType_Select) {
+        [self addSubview:wordView];
+
+        if (type == DWSeedPhraseType_Select) {
             [wordView addTarget:self
                           action:@selector(wordViewAction:)
                 forControlEvents:UIControlEventTouchUpInside];
+
+            wordView.userInteractionEnabled = YES;
+            UIDragInteraction *dragInteraction = [[UIDragInteraction alloc] initWithDelegate:self];
+            dragInteraction.enabled = YES;
+            [wordView addInteraction:dragInteraction];
         }
-        [self addSubview:wordView];
+
         [wordViews addObject:wordView];
     }
     self.wordViews = wordViews;
@@ -176,6 +219,117 @@ static BOOL MasksToBounds(DWSeedPhraseType type) {
             weakSender.userInteractionEnabled = YES;
         }];
     }
+}
+
+#pragma mark - UIDragInteractionDelegate
+
+- (NSArray<UIDragItem *> *)dragInteraction:(UIDragInteraction *)interaction
+                  itemsForBeginningSession:(id<UIDragSession>)session {
+    if (self.hasActiveDragInteractions) {
+        // don't allow multiple drags (with multitouch)
+        return @[];
+    }
+
+    DWSeedWordView *draggingWordView = nil;
+    for (DWSeedWordView *wordView in self.wordViews) {
+        if (wordView.interactions.firstObject == interaction) {
+            draggingWordView = wordView;
+            break;
+        }
+    }
+
+    NSParameterAssert(draggingWordView);
+    if (!draggingWordView) {
+        return @[];
+    }
+
+    DWSeedWordModel *wordModel = draggingWordView.model;
+    if (wordModel.selected) {
+        // don't allow drag of already selected words
+        return @[];
+    }
+
+    // Important notice:
+    // This drag will be handled in the different instance of DWSeedPhraseView
+    // (with type of DWSeedPhraseType_Verify) which adopts UIDropInteractionDelegate.
+    // There is no `delegate` set in that view so we provide *ours* delegate along with the word model
+    DWDragItemLocalObject *localObject = [[DWDragItemLocalObject alloc] initWithWord:wordModel
+                                                                            delegate:self.delegate];
+
+    NSItemProvider *itemProvider = [[NSItemProvider alloc] initWithObject:wordModel.word];
+    UIDragItem *item = [[UIDragItem alloc] initWithItemProvider:itemProvider];
+    item.localObject = localObject;
+
+    return @[ item ];
+}
+
+- (void)dragInteraction:(UIDragInteraction *)interaction sessionWillBegin:(id<UIDragSession>)session {
+    // it's safe to assume that drag begins here since after returning an item from
+    // `dragInteraction:itemsForBeginningSession:` drag may be cancelled and
+    // `dragInteraction:session:didEndWithOperation:` won't be called
+    self.hasActiveDragInteractions = YES;
+}
+
+- (BOOL)dragInteraction:(UIDragInteraction *)interaction sessionIsRestrictedToDraggingApplication:(id<UIDragSession>)session {
+    // in-app drag only
+    return YES;
+}
+
+- (void)dragInteraction:(UIDragInteraction *)interaction
+                session:(id<UIDragSession>)session
+    didEndWithOperation:(UIDropOperation)operation {
+    self.hasActiveDragInteractions = NO;
+}
+
+#pragma mark - UIDropInteractionDelegate
+
+- (BOOL)dropInteraction:(UIDropInteraction *)interaction canHandleSession:(id<UIDropSession>)session {
+    if (!session.localDragSession) {
+        // don't support drops from other apps
+        return NO;
+    }
+
+    // skip check of incoming item since we're not properly implementing NSItemProviderWriting for word models
+    // `-hasItemsConformingToTypeIdentifiers:`, `-canLoadObjectsOfClass:`, etc.
+
+    // restrict d&d to one by one
+    return session.items.count == 1;
+}
+
+- (UIDropProposal *)dropInteraction:(UIDropInteraction *)interaction
+                   sessionDidUpdate:(id<UIDropSession>)session {
+    UIDragItem *dragItem = session.items.firstObject;
+    DWDragItemLocalObject *localObject = (DWDragItemLocalObject *)dragItem.localObject;
+    DWSeedWordModel *wordModel = (DWSeedWordModel *)localObject.wordModel;
+    id<DWSeedPhraseViewDelegate> externalDelegate = localObject.externalDelegate;
+
+    NSAssert([wordModel isKindOfClass:DWSeedWordModel.class], @"Unsupported UIDragItem");
+    NSParameterAssert(externalDelegate);
+
+    BOOL valid = [wordModel isKindOfClass:DWSeedWordModel.class] && externalDelegate;
+    if (!valid) {
+        return [[UIDropProposal alloc] initWithDropOperation:UIDropOperationCancel];
+    }
+
+    BOOL allowed = [externalDelegate seedPhraseView:self allowedToSelectWord:wordModel];
+    // UIDropOperationCopy draws (+) icon above the dragging view, UIDropOperationMove - just the animation
+    UIDropOperation operation = allowed ? UIDropOperationMove : UIDropOperationForbidden;
+
+    UIDropProposal *dropProposal = [[UIDropProposal alloc] initWithDropOperation:operation];
+
+    return dropProposal;
+}
+
+- (void)dropInteraction:(UIDropInteraction *)interaction performDrop:(id<UIDropSession>)session {
+    UIDragItem *dragItem = session.items.firstObject;
+    DWDragItemLocalObject *localObject = (DWDragItemLocalObject *)dragItem.localObject;
+    DWSeedWordModel *wordModel = (DWSeedWordModel *)localObject.wordModel;
+    id<DWSeedPhraseViewDelegate> externalDelegate = localObject.externalDelegate;
+
+    NSAssert([wordModel isKindOfClass:DWSeedWordModel.class], @"Unsupported UIDragItem");
+    NSParameterAssert(externalDelegate);
+
+    [externalDelegate seedPhraseView:self didSelectWord:wordModel];
 }
 
 @end
