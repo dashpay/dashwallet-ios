0380b99ea486007a521d3062f53c3e535f080e96
Quantum Explorer <quantum@dash.org>
2019-07-18 14:11:02 +0700

    Uphold based on current chain

---
+++ DashWallet/UpholdModel/DWUpholdMainnetConstants.m
@@ -0,0 +1,54 @@
+//
+//  Created by Samuel Westrich
+//  Copyright © 2019 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "DWUpholdMainnetConstants.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation DWUpholdMainnetConstants
+
++ (NSString *)authorizeURLFormat {
+    return @"";
+}
+
++ (NSString *)baseURLString {
+    return @"";
+}
+
++ (NSString *)clientID {
+    return @"";
+}
+
++ (NSString *)clientSecret {
+    return @"";
+}
+
++ (NSString *)buyCardURLFormat {
+    return @"";
+}
+
++ (NSString *)transactionURLFormat {
+    return @"";
+}
+
++ (NSString *)logoutURLString {
+    return @"";
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
