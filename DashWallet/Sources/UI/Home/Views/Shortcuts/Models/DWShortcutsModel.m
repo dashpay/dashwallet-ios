690e404f1456f70718cf29f7af68955d52fef630
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-04-26 09:57:24 +0300

    Use Local Currency shortcut instead of Buy/Sell Dash

--- DashWallet/Sources/UI/Home/Views/Shortcuts/Models/DWShortcutsModel.m
+++ DashWallet/Sources/UI/Home/Views/Shortcuts/Models/DWShortcutsModel.m
@@ -82,7 +82,7 @@ static NSInteger MAX_SHORTCUTS_COUNT = 4;
 
     [mutableItems addObject:[DWShortcutAction action:DWShortcutActionType_ScanToPay]];
     [mutableItems addObject:[DWShortcutAction action:DWShortcutActionType_PayToAddress]];
-    [mutableItems addObject:[DWShortcutAction action:DWShortcutActionType_BuySellDash]];
+    [mutableItems addObject:[DWShortcutAction action:DWShortcutActionType_LocalCurrency]];
 
     const BOOL canConfigureShortcuts = !walletNeedsBackup;
     if (canConfigureShortcuts) {
