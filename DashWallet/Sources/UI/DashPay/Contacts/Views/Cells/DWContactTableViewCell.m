659c49774bf85a9987feb70f3027d566b2e2bfb5
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-05-14 12:25:30 +0300

    Contact cell

---
+++ DashWallet/Sources/UI/DashPay/Contacts/Views/Cells/DWContactTableViewCell.m
@@ -0,0 +1,116 @@
+//
+//  Created by administrator
+//  Copyright © 2020 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "DWContactTableViewCell.h"
+
+#import "DWDPAvatarView.h"
+#import "DWUIKit.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+static CGFloat const AVATAR_SIZE = 36.0;
+
+@interface DWContactTableViewCell ()
+
+@property (readonly, nonatomic, strong) DWDPAvatarView *avatarView;
+@property (readonly, nonatomic, strong) UILabel *titleLabel;
+@property (readonly, nonatomic, strong) UILabel *subtitleLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation DWContactTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        self.backgroundColor = [UIColor dw_secondaryBackgroundColor];
+        self.contentView.backgroundColor = self.backgroundColor;
+
+        DWDPAvatarView *avatarView = [[DWDPAvatarView alloc] initWithFrame:CGRectZero];
+        avatarView.translatesAutoresizingMaskIntoConstraints = NO;
+        avatarView.small = YES;
+        [self.contentView addSubview:avatarView];
+        _avatarView = avatarView;
+
+        UILabel *titleLabel = [[UILabel alloc] init];
+        titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        titleLabel.backgroundColor = self.backgroundColor;
+        titleLabel.font = [UIFont dw_fontForTextStyle:UIFontTextStyleCaption1];
+        titleLabel.adjustsFontForContentSizeCategory = YES;
+        titleLabel.textColor = [UIColor dw_darkTitleColor];
+        titleLabel.numberOfLines = 0;
+        [self.contentView addSubview:titleLabel];
+        _titleLabel = titleLabel;
+
+        UILabel *subtitleLabel = [[UILabel alloc] init];
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        subtitleLabel.backgroundColor = self.backgroundColor;
+        subtitleLabel.font = [UIFont dw_fontForTextStyle:UIFontTextStyleCaption2];
+        subtitleLabel.adjustsFontForContentSizeCategory = YES;
+        subtitleLabel.textColor = [UIColor dw_tertiaryTextColor];
+        subtitleLabel.numberOfLines = 0;
+        [self.contentView addSubview:subtitleLabel];
+        _subtitleLabel = subtitleLabel;
+
+        [titleLabel setContentHuggingPriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisVertical];
+        [subtitleLabel setContentHuggingPriority:UILayoutPriorityRequired - 1 forAxis:UILayoutConstraintAxisVertical];
+
+        UILayoutGuide *guide = self.contentView.layoutMarginsGuide;
+        const CGFloat padding = 16.0;
+        const CGFloat spacing = 10.0;
+
+        [NSLayoutConstraint activateConstraints:@[
+            [avatarView.leadingAnchor constraintEqualToAnchor:guide.leadingAnchor],
+            [avatarView.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor],
+            [avatarView.widthAnchor constraintEqualToConstant:AVATAR_SIZE],
+            [avatarView.heightAnchor constraintEqualToConstant:AVATAR_SIZE],
+
+            [titleLabel.topAnchor constraintEqualToAnchor:self.contentView.topAnchor
+                                                 constant:padding],
+            [titleLabel.leadingAnchor constraintEqualToAnchor:avatarView.trailingAnchor
+                                                     constant:spacing],
+            [guide.trailingAnchor constraintEqualToAnchor:titleLabel.trailingAnchor],
+
+            [subtitleLabel.topAnchor constraintEqualToAnchor:titleLabel.bottomAnchor],
+            [subtitleLabel.leadingAnchor constraintEqualToAnchor:avatarView.trailingAnchor
+                                                        constant:spacing],
+            [guide.trailingAnchor constraintEqualToAnchor:subtitleLabel.trailingAnchor],
+            [self.contentView.bottomAnchor constraintEqualToAnchor:subtitleLabel.bottomAnchor
+                                                          constant:padding],
+        ]];
+    }
+    return self;
+}
+
+- (void)setUserDetails:(id<DWUserDetails>)userDetails {
+    _userDetails = userDetails;
+
+    if (userDetails.displayName) {
+        self.titleLabel.text = userDetails.displayName;
+        self.subtitleLabel.text = userDetails.username;
+    }
+    else {
+        self.titleLabel.text = userDetails.username;
+        self.subtitleLabel.text = nil;
+    }
+
+    self.avatarView.username = userDetails.username;
+}
+
+@end
