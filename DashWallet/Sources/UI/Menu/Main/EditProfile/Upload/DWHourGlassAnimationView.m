48ff234421bc86efa749bd1230effb43554246f8
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-11-04 22:42:44 +0300

    Fine tune hourglass animation

--- DashWallet/Sources/UI/Menu/Main/EditProfile/Upload/DWHourGlassAnimationView.m
+++ DashWallet/Sources/UI/Menu/Main/EditProfile/Upload/DWHourGlassAnimationView.m
@@ -19,7 +19,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-static CFTimeInterval const Step = 1.5;
+static CFTimeInterval const Step = 1.2;
 
 @interface DWHourGlassAnimationView ()
 
@@ -69,7 +69,7 @@ NS_ASSUME_NONNULL_END
 }
 
 - (void)performOneStep {
-    CFTimeInterval subStep = 0.3;
+    const CFTimeInterval subStep = 0.235;
 
     CAKeyframeAnimation *keyframeAnimation = [CAKeyframeAnimation animationWithKeyPath:@"contents"];
     keyframeAnimation.values = @[
@@ -80,26 +80,24 @@ NS_ASSUME_NONNULL_END
         (id)[UIImage imageNamed:@"hourglass_5"].CGImage,
     ];
     keyframeAnimation.duration = Step;
-
-    CABasicAnimation *lastValueAnimation = [CABasicAnimation animationWithKeyPath:@"contents"];
-    lastValueAnimation.toValue = (id)[UIImage imageNamed:@"hourglass_5"].CGImage;
-    lastValueAnimation.beginTime = Step;
-    lastValueAnimation.duration = subStep * 2;
+    keyframeAnimation.removedOnCompletion = NO;
+    keyframeAnimation.fillMode = kCAFillModeForwards;
 
     CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
     rotationAnimation.toValue = @(M_PI);
     rotationAnimation.duration = subStep * 2;
     rotationAnimation.beginTime = Step + subStep;
+    rotationAnimation.removedOnCompletion = NO;
+    rotationAnimation.fillMode = kCAFillModeForwards;
 
     CAAnimationGroup *groupAnimation = [CAAnimationGroup animation];
-    groupAnimation.animations = @[ keyframeAnimation, lastValueAnimation, rotationAnimation ];
-    groupAnimation.duration = Step + subStep * 3;
+    groupAnimation.animations = @[ keyframeAnimation, rotationAnimation ];
+    groupAnimation.duration = Step + subStep * 4;
     groupAnimation.beginTime = CACurrentMediaTime();
     groupAnimation.repeatCount = HUGE_VALF;
     groupAnimation.removedOnCompletion = NO;
 
     [self.hgLayer addAnimation:groupAnimation forKey:@"hg_animation"];
-    self.hgLayer.contents = (id)[UIImage imageNamed:@"hourglass_5"].CGImage;
 }
 
 @end
