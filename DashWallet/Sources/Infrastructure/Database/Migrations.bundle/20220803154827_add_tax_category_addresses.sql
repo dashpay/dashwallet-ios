3839033e45165af77875eadeb8919fe66b3ec579
Pavel Tikhonenko <tikhop@gmail.com>
2022-08-17 17:28:55 +0300

    Remove migration

--- DashWallet/Sources/Infrastructure/Database/Migrations.bundle/20220803154827_add_tax_category_addresses.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tax_category_address (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    address text NOT NULL,
-    taxCategory int NOT NULL
-);
-
-CREATE UNIQUE INDEX tax_category_address_address ON tax_category_address (address);
