b85dcf4f3de89857aca51c5355b53e2cac328cd4
Pavel Tikhonenko <tikhop@gmail.com>
2022-09-12 00:59:29 +0200

    When the app isn't running and we get a link thru launchOptions and the current view controller is start view controller then ignore apllication:openURL:options method

--- DashWallet/AppDelegate.m
+++ DashWallet/AppDelegate.m
@@ -178,6 +178,12 @@ performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionH
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    DWStartViewController *startController = (DWStartViewController *)self.window.rootViewController;
+    if ([startController isKindOfClass:DWStartViewController.class]) {
+        startController.deferredURLToProcess = url;
+        return NO;
+    }
+    
     if (![DWURLParser allowsURLHandling]) {
         return NO;
     }
