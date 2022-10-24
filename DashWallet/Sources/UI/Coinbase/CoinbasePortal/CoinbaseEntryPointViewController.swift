229ce8ec06573888e90bfb0ba684360a93ef6426
Pavel Tikhonenko <tikhop@gmail.com>
2022-10-24 21:06:42 +0400

    Implement Coinbase Entry Screen

--- DashWallet/Sources/UI/Coinbase/CoinbasePortal/CoinbaseEntryPointViewController.swift
+++ DashWallet/Sources/UI/Coinbase/CoinbasePortal/CoinbaseEntryPointViewController.swift
@@ -17,17 +17,78 @@
 
 import UIKit
 
-final class CoinbaseEntryPointViewController: UIViewController {
+private enum CoinbaseEntryPointItem: CaseIterable {
+    case buyDash
+    case sellDash
+    case convertCrypto
+    case transferDash
+}
+
+extension CoinbaseEntryPointItem {
+    var title: String
+    {
+        switch self {
+            
+        case .buyDash:
+            return NSLocalizedString("Buy Dash", comment: "Coinbase Entry Point")
+        case .sellDash:
+            return NSLocalizedString("Sell Dash", comment: "Coinbase Entry Point")
+        case .convertCrypto:
+            return NSLocalizedString("Convert Crypto", comment: "Coinbase Entry Point")
+        case .transferDash:
+            return NSLocalizedString("Transfer Dash", comment: "Coinbase Entry Point")
+        }
+    }
+    
+    var description: String {
+        switch self {
+            
+        case .buyDash:
+            return NSLocalizedString("Receive directly into Dash Wallet", comment: "Coinbase Entry Point")
+        case .sellDash:
+            return NSLocalizedString("Receive directly into Coinbase", comment: "Coinbase Entry Point")
+        case .convertCrypto:
+            return NSLocalizedString("Between Dash Wallet and Coinbase", comment: "Coinbase Entry Point")
+        case .transferDash:
+            return NSLocalizedString("Between Dash Wallet and Coinbase", comment: "Coinbase Entry Point")
+        }
+    }
+    
+    var icon: String {
+        switch self {
+            
+        case .buyDash:
+            return "buyCoinbase"
+        case .sellDash:
+            return "sellDash"
+        case .convertCrypto:
+            return "convertCrypto"
+        case .transferDash:
+            return "transferCoinbase"
+        }
+    }
+}
+
+final class CoinbaseEntryPointViewController: BaseViewController {
+    @IBOutlet var connectionStatusView: UIView!
     @IBOutlet var connectionStatusLabel: UILabel!
-    @IBOutlet var dashBalanceLabel: UILabel!
-    @IBOutlet var fiatBalanceLabel: UILabel!
+    @IBOutlet var balanceTitleLabel: UILabel!
+    @IBOutlet var balanceView: BalanceView!
     @IBOutlet var tableView: UITableView!
     @IBOutlet var signOutButton: UIButton!
     @IBOutlet var networkUnavailableView: UIView!
     @IBOutlet var mainContentView: UIView!
     
+    private let items: [CoinbaseEntryPointItem] = CoinbaseEntryPointItem.allCases
+    
+    @IBAction func signOutAction() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureHierarchy()
     }
     
     class func controller() -> CoinbaseEntryPointViewController {
@@ -37,6 +98,62 @@ final class CoinbaseEntryPointViewController: UIViewController {
 
 extension CoinbaseEntryPointViewController {
     private func configureHierarchy() {
+        view.backgroundColor = .dw_secondaryBackground()
         
+        navigationItem.backButtonDisplayMode = .minimal
+        navigationItem.largeTitleDisplayMode = .never
+        
+        networkUnavailableView.isHidden = true
+        
+        connectionStatusView.layer.cornerRadius = 2
+        
+        balanceTitleLabel.text = NSLocalizedString("Dash balance on Coinbase", comment: "Coinbase Entry Point")
+    
+        tableView.isScrollEnabled = false
+        tableView.layer.cornerRadius = 10
+        tableView.clipsToBounds = true
+        tableView.backgroundColor = .white
+        
+        signOutButton.backgroundColor = .white
+        signOutButton.titleLabel?.font = UIFont.dw_font(forTextStyle: .body).withWeight(UIFont.Weight.medium.rawValue)
+        signOutButton.layer.cornerRadius = 10
+        signOutButton.setTitle(NSLocalizedString("Disconnect Coinbase Account", comment: "Coinbase Entry Point"), for: .normal)
+    }
+}
+
+extension CoinbaseEntryPointViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        cell.update(with: items[indexPath.item])
+        cell.separatorInset = .init(top: 0, left: indexPath.item == (items.count - 1) ? 2000 : 63, bottom: 0, right: 0)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 62.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = TransferAmountViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+final class ItemCell: UITableViewCell {
+    @IBOutlet var iconView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var secondaryLabel: UILabel!
+    
+    fileprivate func update(with item: CoinbaseEntryPointItem) {
+        iconView.image = .init(named: item.icon)
+        nameLabel.text = item.title
+        secondaryLabel.text = item.description
     }
 }
---
+++ DashWallet/Sources/UI/Views/BalanceView/BalanceView.swift
@@ -0,0 +1,91 @@
+//  
+//  Created by tkhp
+//  Copyright © 2022 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+final class BalanceView: UIView {
+    public var balance: UInt64 = 0 { //In Dash
+        didSet {
+            reloadView()
+        }
+    }
+    
+    public var dashSymbolColor: UIColor? {
+        didSet {
+            reloadView()
+        }
+    }
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: BalanceView.noIntrinsicMetric, height: 52.0)
+    }
+    
+    private var container: UIStackView!
+    private var dashBalanceLabel: UILabel!
+    private var fiatBalanceLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configureHierarchy()
+    }
+}
+
+private extension BalanceView {
+    private func reloadView() {
+        let balanceColor = UIColor.label
+        let font = UIFont.dw_font(forTextStyle: .title1)
+        let balanceString = NSAttributedString.dw_dashAttributedString(forAmount: balance, tintColor: balanceColor, font: font)
+        dashBalanceLabel.attributedText = balanceString
+    
+        self.fiatBalanceLabel.text = DSPriceManager.sharedInstance().localCurrencyString(forDashAmount: Int64(balance))
+    }
+    
+    private func configureHierarchy() {
+        backgroundColor = .clear
+        
+        self.container = UIStackView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        container.axis = .vertical
+        addSubview(container)
+        
+        self.dashBalanceLabel = UILabel()
+        dashBalanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        dashBalanceLabel.font = .dw_font(forTextStyle: .title1)
+        container.addArrangedSubview(dashBalanceLabel)
+        
+        self.fiatBalanceLabel = UILabel()
+        fiatBalanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        fiatBalanceLabel.font = .dw_font(forTextStyle: .callout)
+        fiatBalanceLabel.textColor = .secondaryLabel
+        container.addArrangedSubview(fiatBalanceLabel)
+        
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: topAnchor),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor),
+            container.leadingAnchor.constraint(equalTo: leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        
+        reloadView()
+    }
+}
