c416c7b24ffbffb693a0fd115fc0c856c49dd7ef
tikhop <tikhop@gmail.com>
2022-12-15 19:35:08 +0700

    feat(explore-dash): Update pre-bundled explore dash database and reorganize the file system structure related to explore db

--- DashWallet/Sources/Models/Explore Dash/Services/ExploreDatabaseSyncManager.swift
+++ DashWallet/Sources/Models/Explore Dash/Services/ExploreDatabaseSyncManager.swift
@@ -26,7 +26,7 @@ private let fileName = "explore"
 private let timestampKey = "Data-Timestamp"
 private let checksumKey = "Data-Checksum"
 
-let bundleExploreDatabaseSyncTime: TimeInterval = 1647448290711/1000
+let bundleExploreDatabaseSyncTime: TimeInterval = 1670906986152/1000
 
 // MARK: - ExploreDatabaseSyncManager
 
