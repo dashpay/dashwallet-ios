524780133e686941a378c4f1b6747d172c95e7d0
Andrei Ashikhmin <synmcj@gmail.com>
2025-06-29 14:35:18 +0700

    feat: piggycards terms

--- DashWallet/Sources/Models/Explore Dash/Model/CTXConstants.swift
+++ DashWallet/Sources/Models/Explore Dash/Model/CTXConstants.swift
@@ -17,6 +17,6 @@
 
 class CTXConstants {
     static let baseURI = "https://spend.ctx.com/"
-    static let ctxGiftCardAgreementUrl = "https://ctx.com/gift-card-agreement/"
+    static let termsAndConditionsUrl = "https://ctx.com/gift-card-agreement/"
     static let supportEmail = "support@ctx.com"
 }
---
+++ DashWallet/Sources/Models/Explore Dash/Model/GiftCardProvider.swift
@@ -0,0 +1,69 @@
+//  
+//  Created by Andrei Ashikhmin
+//  Copyright © 2025 Dash Core Group. All rights reserved.
+//
+//  Licensed under the MIT License (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  https://opensource.org/licenses/MIT
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+enum GiftCardProvider: CaseIterable {
+    case ctx
+    case piggyCards
+    
+    var displayName: String {
+        switch self {
+        case .ctx:
+            return "CTXSpend"
+        case .piggyCards:
+            return "PiggyCards"
+        }
+    }
+    
+    var logoName: String {
+        switch self {
+        case .ctx:
+            return "ctx.logo"
+        case .piggyCards:
+            return "piggycards.logo"
+        }
+    }
+    
+    var termsUrl: String {
+        switch self {
+        case .ctx:
+            return CTXConstants.termsAndConditionsUrl
+        case .piggyCards:
+            return "https://piggy.cards/index.php?route=information/information&information_id=5"
+        }
+    }
+    
+    var supportEmail: String {
+        switch self {
+        case .ctx:
+            return CTXConstants.supportEmail
+        case .piggyCards:
+            return "" // TODO: Confirm correct support email
+        }
+    }
+    
+    func isUserSignedIn() -> Bool {
+        switch self {
+        case .ctx:
+            return CTXSpendService.shared.isUserSignedIn
+        case .piggyCards:
+            // TODO: Implement PiggyCards authentication
+            return false
+        }
+    }
+}
