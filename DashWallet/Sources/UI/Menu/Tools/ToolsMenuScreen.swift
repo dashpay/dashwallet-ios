9c6a3aa0ddd6ad66c34f6199e2c9dac04f119d19
HashEngineering <hashengineeringsolutions@gmail.com>
2025-09-15 11:26:16 -0700

    feat: release 8.4.1 (#728)
    * chore: update translations
    
    * fix: use correct name for Image.face.id icon
    
    * chore: update project file and set version to 8.4.1
    
    * fix: set back icon foreground color to .primary

--- DashWallet/Sources/UI/Menu/Tools/ToolsMenuScreen.swift
+++ DashWallet/Sources/UI/Menu/Tools/ToolsMenuScreen.swift
@@ -44,7 +44,7 @@ struct ToolsMenuScreen: View {
                 }) {
                     Image(systemName: "chevron.left")
                         .font(.system(size: 18, weight: .medium))
-                        .foregroundColor(.black)
+                        .foregroundColor(.primary)
                         .frame(width: 36, height: 36)
                         .overlay(
                             Circle().stroke(Color.gray300.opacity(0.3), lineWidth: 1)
